97674e144719efb2cc16a5ba6fbb3334f4c83280
Rhayann Schuvantek <rhayannbianco@gmail.com>
2024-02-20 20:57:43 -0300

    creating new endpoint

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,19 @@ app.get('/:cityName', async (req, res) => {
       res.status(500).json({ error: 'Erro ao obter dados do clima' });
     }
   });
+
+  app.get('/weather/:cityName', async (req, res) => {
+    try {
+      const city = req.params.cityName; 
+  
+      const response = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=7&aqi=no&alerts=yes`);
+      
+      res.json(response.data);
+    } catch (error: any) {
+      console.error('Erro ao obter dados do clima:', error.message);
+      res.status(500).json({ error: 'Erro ao obter dados do clima' });
+    }
+  });
   
 
 app.listen(port, () => {
